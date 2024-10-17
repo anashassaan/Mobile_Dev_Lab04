e4a839e172c5c59d6a363b9e97e5bb5f40afa1b8
Muhammad Anas Hassaan <115940875+anashassaan@users.noreply.github.com>
2024-10-17 12:47:19 +0500

    updated

--- lab_task04/lib/lab_task_q3.dart
+++ lab_task04/lib/lab_task_q3.dart
@@ -1,60 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(my_linkedin());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class my_linkedin extends StatelessWidget {
+  const my_linkedin({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Center(child: Text('App Bar')),
-        ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
+          title: Row(
             children: [
-              customContainer(color: const Color.fromARGB(255, 124, 99, 99)),
-              customContainer(color: const Color.fromARGB(31, 112, 49, 49)),
-              customContainer(color: const Color.fromARGB(255, 222, 123, 156)),
               Container(
-                width: 300,
-                height: 300,
-                decoration: BoxDecoration(color: Colors.red),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Column(
-                    children: [
-                      customContainer(color: Colors.blue),
-                      customContainer(color: Colors.red),
-                    ],
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.black12, shape: BoxShape.circle),
+                // child: Image(image: AssetImage('assests/abc.jpeg')),
+              ),
+              Container(
+                width: 230,
+                height: 50,
+                margin: EdgeInsets.only(left: 15),
+                decoration: BoxDecoration(color: Colors.black26),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 35,
+                  height: 35,
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(97, 55, 54, 54)),
+                  child: Icon(
+                    Icons.add,
+                    size: 35,
+                    color: Colors.white,
                   ),
                 ),
               ),
+              Container(
+                width: 30,
+                height: 30,
+                child: TextButton(onPressed: () {}, child: Icon(Icons.message)),
+              )
             ],
           ),
         ),
-      ),
-    );
-  }
-
-  Widget customContainer({required Color color, IconData icon = Icons.abc}) {
-    return Container(
-      padding: const EdgeInsets.all(8),
-      width: 300,
-      height: 300,
-      decoration: BoxDecoration(
-        color: color,
-        shape: BoxShape.rectangle,
-      ),
-      child: Icon(
-        icon,
-        size: 50,
       ),
     );
   }
