e4a0afb399ac728586c2ba2ed859e06a8de6a06d
Muhammad Anas Hassaan <115940875+anashassaan@users.noreply.github.com>
2024-10-18 19:40:26 +0500

    Update lab05_tas_q1k.dart

--- lab_task04/lib/lab05_tas_q1k.dart
+++ lab_task04/lib/lab05_tas_q1k.dart
@@ -11,29 +11,82 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool _showColumn = false;
+
+  void _onLongPress() {
+    setState(() {
+      _showColumn = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Color Change & Fade Demo'),
+          title: const Text('Animation Lab'),
         ),
         body: Padding(
           padding: const EdgeInsets.all(16),
-          child: Center(
-              child: BounceInDown(
-            delay: Duration(seconds: 2),
-            child: Container(
-              height: 200,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                color: Colors.green,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(
+                      () {
+                        _showColumn = false;
+                      },
+                    );
+                    // Star bounce down animation
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return Center(
+                          child: BounceInDown(
+                            child: CustonIcon(),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  onLongPress: _onLongPress,
+                  child: Text('Long Press'),
+                ),
               ),
-            ),
-          )),
+              if (_showColumn)
+                Column(
+                  children: [
+                    FadeInDown(
+                        child: Icon(Icons.bike_scooter_rounded,
+                            size: 100, color: Colors.blue)),
+                    SlideInLeft(
+                        child: Icon(Icons.hub_sharp,
+                            size: 100, color: Colors.brown)),
+                    BounceInUp(
+                        child: Icon(Icons.baby_changing_station_rounded,
+                            size: 100, color: Colors.orange)),
+                  ],
+                ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class CustonIcon extends StatelessWidget {
+  const CustonIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      Icons.star_rate_outlined,
+      size: 100,
+      color: Colors.amber,
     );
   }
 }
